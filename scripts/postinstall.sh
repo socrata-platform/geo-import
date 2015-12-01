1999d0e1a6d03f3b2311536a39a197ed7d792bfc
Chris Duranti <chris.duranti@socrata.com>
2015-12-01 11:45:33 -0800

    Build issues
    * Move postinstall to scripts dir
    * update readme with explicit CC and CXX env vars
    * update package.json metadata

---
+++ scripts/postinstall.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+if [ ! -d "../../node-srs" ]; then
+  git clone https://github.com/mapbox/node-srs ../../node-srs
+fi
+pushd ../../node-srs
+echo $(pwd)
+rm -r node_modules
+CC=gcc CXX=g++ npm install --build-from-source
+popd
+ln -s $(pwd)/../node-srs node_modules/node-srs
