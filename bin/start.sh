fff0f27c4ab9749755605a2131617392de4314ad
Thomas Brigham <thomas@thomasbrigham.me>
2018-09-13 14:29:04 -0700

    Add solo config; update start script

--- bin/start.sh
+++ bin/start.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 set -e
 
+THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd ${THIS_DIR}/..
+
 make
 
 node_version="`cat .node-version`"
-if [ -n "`command -v nvm`" ]
-then
+if [ -n "`command -v nvm`" ]; then
   nvm install $node_version
   nvm use $node_version
-else
+elif [ -n "`command -v n`" ]; then
   n $node_version
 fi
 
