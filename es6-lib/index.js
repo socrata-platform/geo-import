aaeb713b94b05c82fa70a66ef2b795d2e727300c
rozap <chrisd1891@gmail.com>
2019-04-30 11:57:22 -0700

    upgrade node to 10.x.x

--- es6-lib/index.js
+++ es6-lib/index.js
@@ -1,11 +1,6 @@
 import service from './service';
 import config from './config';
 import ZKClient from './upstream/zookeeper';
-import SegfaultHandler from 'segfault-handler';
-import ofe from 'ofe';
-
-ofe.call();
-SegfaultHandler.registerHandler("crash.log");
 
 var conf = config();
 var zk = new ZKClient(conf);
